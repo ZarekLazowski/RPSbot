3ff89cd7db8efe8812749bac647887334d2bf65d
ZarekLazowski <officiallyzarek@gmail.com>
2022-06-07 23:33:59 -0700

    rps plays to best of 3

--- src/bot.py
+++ src/bot.py
@@ -19,25 +19,64 @@ class RPSbot(commands.Bot):
             # Start DM with user
             dm_channel = await context.author.create_dm()
 
-            await dm_channel.send('Rock, Paper, Scissors, Shoot!')
+            await dm_channel.send('Best of three! Rock, Paper, Scissors, Shoot!')
 
-            try:
+            # Stat bookkeeping
+            wins = 0
+            loss = 0
+            game = 1
+
+            # Keep playing until user or bot wins twice
+            while wins < 2 and loss < 2:
+                if wins == loss and game == 1:
+                    await dm_channel.send('Game 1')
+                else:
+                    await dm_channel.send(str(wins) + ' to ' + str(loss) + '! Game ' + str(game))
+
                 def check(msg):
-                    return any(opt in msg.content.lower() for opt in rps.rps_choice) and msg.channel == dm_channel
+                    return any(opt in msg.content.lower() for opt in rps.rps_keyword) \
+                        and msg.channel == dm_channel \
+                        and msg.author != self.user
 
                 # Wait for user to send message
                 with dm_channel.typing():
-                    # Grab message when received, timeout after 30 sec
-                    msg = await self.wait_for('message', check=check, timeout=30)
+                    try:
+                        # Grab message when received, timeout after 30 sec
+                        msg = await self.wait_for('message', check=check, timeout=30)
+
+                    # On timeout, mark loss and exit loop
+                    except(asyncio.TimeoutError):
+                        await dm_channel.send('You took too long. This counts as forfeit')
+                        loss = 2
+                        break
+
+                    if msg.content.lower() == 'quit':
+                        await dm_channel.send('Coward')
+                        break
+
+                    # Get RPS response
+                    resp = rps.rps_rand()
 
                     # Send its choice
-                    await dm_channel.send(rps.rps_rand())
+                    await dm_channel.send(resp)
 
-                    # TODO: check to see if it won or not
-            
-            except(asyncio.TimeoutError):
-                await dm_channel.send('You took too long')
-                print('Timeout occured')
+                # Check winner
+                res = rps.rps_winner(resp, msg.content.lower())
+
+                # Do thing based on winner
+                if res == 'bot':
+                    loss += 1
+                elif res == 'usr':
+                    wins += 1
+                    
+                # Increment game counter
+                game += 1
+
+            # Post game wrapup. TODO: implement per user stat tracking
+            if loss == 2:
+                await dm_channel.send('ggs, better luck next time')
+            elif wins == 2:
+                await dm_channel.send('ggs, I\'ll train harder')
         
         # Flip a coin for the user
         @self.command(name='flip')
--- src/rps.py
+++ src/rps.py
@@ -1,10 +1,30 @@
 import random
 
+rps_keyword = ("rock", "paper", "scissors", "quit")
+
 rps_choice = ("rock", "paper", "scissors")
 coin_choice = ("Heads", "Tails")
 
 def rps_rand() -> str:
-    return rps_choice[random.randint(0,2)]
+    return random.choice(rps_choice)
+
+def rps_winner(bot: str, usr: str) -> str:
+    # Convert string to indices
+    usr = rps_choice.index(usr.lower())
+    bot = rps_choice.index(bot.lower())
+
+    # Determine winner
+    # Tie
+    if usr == bot:
+        return 'tie'
+
+    # User wins
+    elif bot == ((usr + 2) % 3):
+        return 'usr'
+
+    # Bot wins
+    else:
+        return 'bot'
 
 def coin_flip() -> str:
-    return coin_choice[random.randint(0,1)]
+    return random.choice(coin_choice)
