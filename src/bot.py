a768a4305c9eb0bc5b9c6dca5fe7c2d3e70f05bd
ZarekLazowski <officiallyzarek@gmail.com>
2022-06-08 13:25:49 -0700

    Fixed a bug with the scissors emote not being caught by the check command

--- src/bot.py
+++ src/bot.py
@@ -12,6 +12,83 @@ class RPSbot(commands.Bot):
     async def on_ready(self):
         print(f'{self.user} has connected to Discord')
 
+    async def do_rps_game(self, dm_channel) -> str:
+        # Stat bookkeeping
+        wins = 0
+        loss = 0
+        game = 1
+
+        # Display introduction text
+        pretext = rps.rps_intro
+
+        # Keep playing until user or bot wins twice
+        while wins < 2 and loss < 2:
+            if wins == loss and game == 1:
+                pretext += 'Game 1'
+            else:
+                pretext += str(wins) + ' to ' + str(loss) + '! Game ' + str(game)
+
+            await dm_channel.send(pretext)
+
+            # Check if a received command is valid
+            def check(msg):
+                if msg.content.isalpha():
+                    return (msg.content.lower() in rps.rps_keyword) \
+                        and msg.channel == dm_channel \
+                        and msg.author != self.user
+                else:
+                    return (msg.content[0] in rps.rps_keyword) \
+                    and msg.channel == dm_channel \
+                    and msg.author != self.user
+
+            # Wait for user to send message
+            with dm_channel.typing():
+                try:
+                    # Grab message when received, timeout after 30 sec
+                    msg = await self.wait_for('message', check=check, timeout=30)
+
+                # On timeout, mark loss and exit loop
+                except(asyncio.TimeoutError):
+                    await dm_channel.send('You took too long. This counts as forfeit')
+                    loss = 2
+                    break
+
+                if msg.content.lower() == 'quit':
+                    await dm_channel.send('Coward')
+                    break
+
+                # Respond with same style (emote vs word)
+                if msg.content.isalpha():
+                    resp = rps.rps_rand()
+                else:
+                    resp = rps.rps_erand()
+                
+                # Send its choice
+                await dm_channel.send(resp)
+
+            # Check winner
+            res = rps.rps_winner(resp, msg.content)
+
+            # Do thing based on winner
+            if res == 'bot':
+                loss += 1
+            elif res == 'usr':
+                wins += 1
+                
+            # Increment game counter
+            game += 1
+
+            # Clear pretext
+            pretext = ''
+        
+        # Report who won and who lost
+        if wins == 2:
+            await dm_channel.send('ggs, I\'ll train harder')
+            return 'win'
+        else:
+            await dm_channel.send('ggs, better luck next time')
+            return 'lose'
+
     def command_setup(self):
         # Play Rock, Paper, Scissors with a user in their DMs
         @self.command(name='rps')
@@ -19,73 +96,16 @@ class RPSbot(commands.Bot):
             # Start DM with user
             dm_channel = await context.author.create_dm()
 
-            # Stat bookkeeping
-            wins = 0
-            loss = 0
-            game = 1
+            # Do the game in DMs
+            res = await self.do_rps_game(dm_channel)
 
-            # Display introduction text
-            pretext = rps.rps_intro
+            # Report back to the guild who won/lost
+            if res == 'lose':
+                report = context.author.display_name + ' is a loser'
+            else:
+                report = context.author.display_name + ' won this time'
 
-            # Keep playing until user or bot wins twice
-            while wins < 2 and loss < 2:
-                if wins == loss and game == 1:
-                    pretext += 'Game 1'
-                else:
-                    pretext += str(wins) + ' to ' + str(loss) + '! Game ' + str(game)
-
-                await dm_channel.send(pretext)
-
-                def check(msg):
-                    return any(opt in msg.content.lower() for opt in rps.rps_keyword) \
-                        and msg.channel == dm_channel \
-                        and msg.author != self.user
-
-                # Wait for user to send message
-                with dm_channel.typing():
-                    try:
-                        # Grab message when received, timeout after 30 sec
-                        msg = await self.wait_for('message', check=check, timeout=30)
-
-                    # On timeout, mark loss and exit loop
-                    except(asyncio.TimeoutError):
-                        await dm_channel.send('You took too long. This counts as forfeit')
-                        loss = 2
-                        break
-
-                    if msg.content.lower() == 'quit':
-                        await dm_channel.send('Coward')
-                        break
-
-                    # Respond with same style (emote vs word)
-                    if msg.content.isalpha():
-                        resp = rps.rps_rand()
-                    else:
-                        resp = rps.rps_erand()
-                    
-                    # Send its choice
-                    await dm_channel.send(resp)
-
-                # Check winner
-                res = rps.rps_winner(resp, msg.content)
-
-                # Do thing based on winner
-                if res == 'bot':
-                    loss += 1
-                elif res == 'usr':
-                    wins += 1
-                    
-                # Increment game counter
-                game += 1
-
-                # Clear pretext
-                pretext = ''
-
-            # Post game wrapup. TODO: implement per user stat tracking
-            if loss == 2:
-                await dm_channel.send('ggs, better luck next time')
-            elif wins == 2:
-                await dm_channel.send('ggs, I\'ll train harder')
+            await context.channel.send(report)
         
         # Flip a coin for the user
         @self.command(name='flip')
